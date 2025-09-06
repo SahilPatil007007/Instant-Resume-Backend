1a1c0bd6302d7e64415ec8b2a1efffcd3ad0ca00
Sahil Patil <sahilpatil221004@gmail.com>
2025-09-06 18:23:41 +0530

    Changed Frontend Adress

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
     ? process.env.ALLOWED_ORIGINS.split(',') 
     : [
         "http://localhost:5173",
-        "http://localhost:5174"
+        "https://resumeai-in.netlify.app"
     ];
 
 app.use(cors({
